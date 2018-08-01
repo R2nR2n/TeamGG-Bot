d7726ae415461c51197fcc1813ed980fc170d5f4
R2nR2n <37939025+R2nR2n@users.noreply.github.com>
2018-08-01 19:04:47 -0400

    setup and onready

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const discord = require('discord.js');
+
+const client = Discord.Client();
+
+const config = require('./config.json');
+
+client.on("ready", () => { 
+  console.log('Bot has successfully started in ${client.guilds.size}');
+  client.user.setActivity('Prefix is $');
+});
